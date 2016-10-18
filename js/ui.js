173070da8df7fbaa7bbd802d99961e064cc38bfe
Evolution0 <xxanthonykanexx@gmail.com>
2016-10-18 12:27:48 -0400

    TODO + Oopsie
    Looks like I didn't even notice an incredibly noob mistake years ago
    (Not terminating tags) thanks to
    https://github.com/Superraiden/Startpage/commit/48c555f3102e0c55e1d1566709929203d4aeafcb
    for noticing long after the fact haha

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,12 @@
 // use a custom userChrome.css that modifies
 // the browsers default content display.
 // UI mods should not affect it.
+
+// TODO: CLI commands
+// TODO: Search provider switcher (Google, Duck Duck Go, Yahoo, Wikipedia, Reddit, Wolfram Alpha, etc)
+// TODO: Add easter eggs back in from later iteration that was lost years ago.
+// TODO: Look into alternate themes (Old CRT terminal emulator theme?)
+
 $(document).ready(function() {
 	$( "#container" ).draggable().position({
 	my: "center",
